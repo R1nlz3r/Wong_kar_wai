29f147d987462592ab85a4949e31e0ec46fcca66
Mathis Pandelé <mapandel@student.42.fr>
2017-03-18 17:19:47 +0100

    game.c 1.0 : Implementation

---
+++ sources/game.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/18 16:40:30 by mapandel          #+#    #+#             */
+/*   Updated: 2017/03/18 17:17:01 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game_2048.h"
+
+void		game(t_2048 *wkw)
+{
+	int		key;
+
+	key = 0;
+	while (1)
+	{
+		clear();
+		wkw = game_new_piece(wkw);
+		//afficher la grille
+		key = getch();
+		//if (tmp == KEY_UP)
+			//game_change_up();
+		//else if (tmp == KEY_DOWN)
+			//game_change_down();
+		//else if (tmp == KEY_RIGHT)
+			//game_change_right();
+		//else if (tmp == KEY_LEFT)
+			//game_change_left();
+		if (key == 27 || check_end_game(wkw)) //else if
+			break ;
+	}
+}
