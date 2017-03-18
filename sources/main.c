ebe15ae5d64b6d2afaec1fbdd66fb3bb69a8d2ce
Mathis Pandelé <mapandel@student.42.fr>
2017-03-18 17:22:32 +0100

    main.c 1.1 : Fonctionnel

--- sources/main.c
+++ sources/main.c
@@ -6,51 +6,53 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 10:49:40 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/18 12:03:08 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/18 17:16:35 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
 
-static t_2048		*init_t_2048(t_2048 *wong_kar_wai)
+static t_2048		*init_t_2048(t_2048 *wkw)
 {
-	if (!(wong_kar_wai = ft_memalloc(sizeof(t_2048))))
+	int		tmp;
+
+	tmp = 0;
+	if (!(wkw = ft_memalloc(sizeof(t_2048)))
+		|| !(wkw->map = ft_memalloc(sizeof(int**) * 4)))
 		return (NULL);
-	wong_kar_wai->win_value = WIN_VALUE;
-	return (wong_kar_wai);
+	while (tmp < 4)
+		wkw->map[tmp++] = ft_tabnew(4);
+	wkw->win_value = WIN_VALUE;
+	wkw->lines = 0;
+	wkw->columns = 0;
+	return (wkw);
 }
 
-static void 		del_t_2048(t_2048 *wong_kar_wai)
+static void 		del_t_2048(t_2048 *wkw)
 {
-	ft_memdel((void**)wong_kar_wai->wdow);
-	ft_memdel((void**)wong_kar_wai);
+	int		tmp;
+
+	tmp = 0;
+	while (tmp < 4)
+		ft_memdel((void**)&wkw->map[tmp++]);
+	ft_memdel((void**)wkw->map);
+	ft_memdel((void**)wkw->wdow);
+	ft_memdel((void**)wkw);
 }
 
 int					main(void)
 {
-	t_2048		*wong_kar_wai;
-	int 		tmp = 0;
+	t_2048		*wkw;
 
-	wong_kar_wai = NULL;
-	if (!(wong_kar_wai = init_t_2048(wong_kar_wai))
-		|| wong_kar_wai->win_value % 2 != 0)
+	wkw = NULL;
+	if (!(wkw = init_t_2048(wkw))
+		|| wkw->win_value % 2 != 0)
 		return (-1);
+	srand((unsigned)time(NULL));
 	initscr();
 	noecho();
-
-	//init le jeu
-
-	while (1)
-	{
-		clear();
-		mvprintw(LINES / 2, COLS / 2, ft_itoa(tmp));
-		tmp = getch();
-		if (tmp == 27)
-			break;
-		//edit le jeu selon l'input
-	}
-
+	game(wkw);
 	endwin();
-	del_t_2048(wong_kar_wai);
+	del_t_2048(wkw);
 	return (0);
 }
