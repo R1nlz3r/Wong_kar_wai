199f373a3fdc0660912482c1b7276e2db63e583e
Mourad DARDAKH <mdardakh@e2r2p20.42.fr>
2017-03-18 21:09:09 +0100

    game_new_piece.c 1.1 : Ajout de fonctions

--- sources/game_new_piece.c
+++ sources/game_new_piece.c
@@ -12,6 +12,38 @@
 
 #include "game_2048.h"
 
+static int check_end(t_2048 *wkw)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (wkw->map[i][j] == 0)
+				return (0);
+				j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+static void 		del_t_2048(t_2048 *wkw)
+{
+	int		tmp;
+
+	tmp = 0;
+	while (tmp < 4)
+		ft_memdel((void**)&wkw->map[tmp++]);
+	ft_memdel((void**)wkw->map);
+	free(wkw->wdow);
+	free(wkw);
+}
+
 t_2048		*game_new_piece(t_2048 *wkw)
 {
 	int		tmp;
@@ -19,6 +51,11 @@ t_2048		*game_new_piece(t_2048 *wkw)
 	tmp = 2;
 	while (1)
 	{
+		if (check_end(wkw) == 1)
+		{
+			del_t_2048(wkw);
+			return (NULL);
+		}
 		wkw->lines = rand() % 4;
 		wkw->columns = rand() % 4;
 		if (wkw->map[wkw->lines][wkw->columns] == 0)
