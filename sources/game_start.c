6340c788d6604855200d00ce5467859b2274fa8a
Mathis Pandelé <mapandel@student.42.fr>
2017-03-19 16:07:18 +0100

    game_start 1.1 : Amelioration

--- sources/game_start.c
+++ sources/game_start.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 14:09:49 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 15:29:49 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/19 15:58:15 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		game_start(t_2048 *wkw)
 		wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
 		wborder(wkw->wdow, '/','/',  '/' , '/', '/','/', '/','/');
 		move(LINES * 0.5, COLS * 0.5 - ft_strlen("Welcome to 2048 ! \
-			Press Enter to play") / 2);
+			Press Enter to play") / 2 + 2);
 		printw("Welcome to 2048 ! Press Enter to play");
 		key = getch();
 		if (key == KEY_UP)
