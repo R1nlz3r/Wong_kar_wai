1351dc0f03afa0786f9b5f639256850b6be7bf5b
Mathis Pandelé <mapandel@student.42.fr>
2017-03-19 15:39:10 +0100

    game_start 1.0 : Implementation

---
+++ sources/game_start.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_start.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/19 14:09:49 by mapandel          #+#    #+#             */
+/*   Updated: 2017/03/19 15:29:49 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game_2048.h"
+
+int		game_start(t_2048 *wkw)
+{
+	int		key;
+
+	while (1)
+	{
+		clear();
+		wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
+		wborder(wkw->wdow, '/','/',  '/' , '/', '/','/', '/','/');
+		move(LINES * 0.5, COLS * 0.5 - ft_strlen("Welcome to 2048 ! \
+			Press Enter to play") / 2);
+		printw("Welcome to 2048 ! Press Enter to play");
+		key = getch();
+		if (key == KEY_UP)
+			break ;
+		else if (key == 27)
+			return (1);
+	}
+	return (0);
+}
