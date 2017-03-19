6b6bafa252b7323baca33c70f73760b0430384e1
Mathis Pandelé <mapandel@student.42.fr>
2017-03-19 15:38:40 +0100

    game_2048.h 1.5 : Norme

--- sources/game_2048.h
+++ sources/game_2048.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 10:47:07 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/18 17:28:06 by mdardakh         ###   ########.fr       */
+/*   Updated: 2017/03/19 15:34:04 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <time.h>
 # include "../libft/libft.h"
 
-# define FOND 9
-# define CHIFFRE 10
-# define ZERO 11
-# define DEUX 12
-# define QUATRE 13
-# define HUIT 14
+# define FOND		9
+# define CHIFFRE	10
+# define ZERO		11
+# define DEUX		12
+# define QUATRE		13
+# define HUIT		14
 
 enum e_const
 {
@@ -37,20 +37,18 @@ typedef struct		s_2048
 	int				**map;
 	int				lines;
 	int				columns;
+	size_t			*score;
 }					t_2048;
 
-int				game(t_2048 *wkw);
-t_2048			*game_new_piece(t_2048 *wong_kar_wai);
-int				check_end_game(t_2048 *wkw);
-void			draw_window(t_2048 *wkw);
-int 			game_over(void);
-
-void 			colo(void);
-void 			start(t_2048 *wkw);
-short 			get_color(short val);
-int				game_down(t_2048 *wkw);
-int				game_up(t_2048 *wkw);
-int				game_left(t_2048 *wkw);
-int				game_right(t_2048 *wkw);
+int					game(t_2048 *wkw);
+void				game_new_piece(t_2048 *wkw);
+int					check_end_game(t_2048 *wkw);
+void				draw_window(t_2048 *wkw);
+int 				game_over(void);
+int 				game_start(t_2048 *wkw);
+int					game_down(t_2048 *wkw);
+int					game_up(t_2048 *wkw);
+int					game_left(t_2048 *wkw);
+int					game_right(t_2048 *wkw);
 
 #endif
