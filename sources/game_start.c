a0ce2f345e21c1960d49954539bc3cdf273df793
Mathis PANDELE <mapandel@e2r2p22.42.fr>
2017-03-19 21:54:21 +0100

    game_start.c 1.4 : Norme

--- sources/game_start.c
+++ sources/game_start.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 14:09:49 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 20:09:52 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/19 21:09:18 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		game_start(t_2048 *wkw)
 		else
 		{
 			wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
-			wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
+			wborder(wkw->wdow, '|', '|', '-', '-', '+', '+', '+', '+');
 			move(LINES * 0.5, COLS * 0.5 - ft_strlen("Welcome to 2048 ! \
 				Press Enter to play") / 2 + 2);
 			printw("Welcome to 2048 ! Press Enter to play");
@@ -36,7 +36,7 @@ int		game_start(t_2048 *wkw)
 		if (key == 10)
 			break ;
 		else if (key == 27)
-			return (1);
+			return (0);
 	}
-	return (0);
+	return (1);
 }
