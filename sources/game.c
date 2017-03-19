1190ca774a643785d4dc053bc2afffea8520dd84
Mathis Pandelé <mapandel@student.42.fr>
2017-03-19 20:18:57 +0100

    game.c 1.7 : Secure window size

--- sources/game.c
+++ sources/game.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 16:40:30 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 19:41:28 by mdardakh         ###   ########.fr       */
+/*   Updated: 2017/03/19 20:18:00 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,26 @@ static int		aff_win(t_2048 *wkw)
 	while (1)
 	{
 		clear();
-		wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
-		wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
-		move(LINES * 0.5 - 2, COLS * 0.5 - 4);
-		init_pair(5, COLOR_GREEN, COLOR_BLACK);
-		attron(COLOR_PAIR(5));
-		printw("YOU WIN");
-		attroff(COLOR_PAIR(5));
-		move(LINES / 2, COLS / 2 - 34);
-		printw("Partie terminee, "
-			"appuyez sur ENTER pour continuer ou ESC pour quitter.");
-		move(LINES * 0.5 + 2, COLS * 0.5 - 5);
-		printw("Score : %u", wkw->score);
+		if (LINES < 20 || COLS < 80)
+		{
+			clear();
+			printw("Window too small, fuck you.");
+		}
+		else
+		{
+			wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
+			wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
+			move(LINES * 0.5 - 2, COLS * 0.5 - 4);
+			init_pair(5, COLOR_GREEN, COLOR_BLACK);
+			attron(COLOR_PAIR(5));
+			printw("YOU WIN");
+			attroff(COLOR_PAIR(5));
+			move(LINES / 2, COLS / 2 - 34);
+			printw("Partie terminee, "
+				"appuyez sur ENTER pour continuer ou ESC pour quitter.");
+			move(LINES * 0.5 + 2, COLS * 0.5 - 5);
+			printw("Score : %u", wkw->score);
+		}
 		key = getch();
 		if (key == 10)
 			break ;
@@ -87,23 +95,31 @@ static int		aff_game_over(t_2048 *wkw)
 	while (1)
 	{
 		clear();
-		wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
-		wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
-		move(LINES * 0.5 - 2, COLS * 0.5 - 4);
-		init_pair(5, COLOR_RED, COLOR_BLACK);
-		attron(COLOR_PAIR(5));
-		printw("GAME OVER.");
-		attroff(COLOR_PAIR(5));
-		move(LINES / 2, COLS / 2 - 34);
-		printw("Partie terminee, "
-			"appuyez sur ENTER pour rejouez ou ESC pour quitter.");
-		move(LINES * 0.5 + 2, COLS * 0.5 - 5);
-		printw("Score : %u", wkw->score);
+		if (LINES < 20 || COLS < 80)
+		{
+			clear();
+			printw("Window too small, fuck you.");
+		}
+		else
+		{
+			wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
+			wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
+			move(LINES * 0.5 - 2, COLS * 0.5 - 4);
+			init_pair(5, COLOR_RED, COLOR_BLACK);
+			attron(COLOR_PAIR(5));
+			printw("GAME OVER.");
+			attroff(COLOR_PAIR(5));
+			move(LINES / 2, COLS / 2 - 34);
+			printw("Partie terminee, "
+				"appuyez sur ENTER pour rejouez ou ESC pour quitter.");
+				move(LINES * 0.5 + 2, COLS * 0.5 - 5);
+			printw("Score : %u", wkw->score);
+		}
 		key = getch();
 		if (key == 10)
-			break ;
-		else if (key == 27)
 			return (1);
+		else if (key == 27)
+			break ;
 	}
 	return (0);
 }
