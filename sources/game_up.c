b5b159c6e90eea3f822ab1837d190a22990f3769
Mathis PANDELE <mapandel@e2r2p22.42.fr>
2017-03-19 21:54:04 +0100

    game_up.c 1.2 : Norme

--- sources/game_up.c
+++ sources/game_up.c
@@ -6,13 +6,38 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 18:56:03 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 16:21:24 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/19 21:40:49 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
 
-int		game_up(t_2048 *wkw)
+static void		game_up2(t_2048 *wkw, int *tmp, int *count)
+{
+	if (*tmp < 0 && (++count))
+	{
+		wkw->map[0][wkw->columns] = wkw->map[wkw->lines]
+			[wkw->columns];
+		wkw->map[wkw->lines][wkw->columns] = 0;
+	}
+	else if (wkw->map[*tmp][wkw->columns] == wkw->map[wkw->lines]
+		[wkw->columns] && (++*count))
+	{
+		wkw->map[*tmp][wkw->columns] = wkw->map[wkw->lines]
+			[wkw->columns] * 2;
+		wkw->score += (size_t)wkw->map[wkw->lines]
+			[wkw->columns] * 2;
+		wkw->map[wkw->lines][wkw->columns] = 0;
+	}
+	else if (*tmp + 1 != wkw->lines && (++*count))
+	{
+		wkw->map[*tmp + 1][wkw->columns] = wkw->map[wkw->lines]
+			[wkw->columns];
+		wkw->map[wkw->lines][wkw->columns] = 0;
+	}
+}
+
+int				game_up(t_2048 *wkw)
 {
 	int		tmp;
 	int		count;
@@ -29,23 +54,7 @@ int		game_up(t_2048 *wkw)
 				tmp = wkw->lines - 1;
 				while (tmp >= 0 && wkw->map[tmp][wkw->columns] == 0)
 					--tmp;
-				if (tmp < 0 && (++count))
-				{
-					wkw->map[0][wkw->columns] = wkw->map[wkw->lines][wkw->columns];
-					wkw->map[wkw->lines][wkw->columns] = 0;
-				}
-				else if (wkw->map[tmp][wkw->columns] == wkw->map[wkw->lines][wkw->columns] && (++count))
-				{
-					wkw->map[tmp][wkw->columns] = wkw->map[wkw->lines][wkw->columns] * 2;
-					wkw->score += (size_t)wkw->map[wkw->lines]
-						[wkw->columns] * 2;
-					wkw->map[wkw->lines][wkw->columns] = 0;
-				}
-				else if (tmp + 1 != wkw->lines && (++count))
-				{
-					wkw->map[tmp + 1][wkw->columns] = wkw->map[wkw->lines][wkw->columns];
-					wkw->map[wkw->lines][wkw->columns] = 0;
-				}
+				game_up2(wkw, &tmp, &count);
 			}
 			++wkw->lines;
 		}
