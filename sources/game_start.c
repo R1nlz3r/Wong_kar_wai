905cbfb444e8389ac22d02a92aaa3bde2e8e4a0f
Mathis Pandelé <mapandel@student.42.fr>
2017-03-19 17:58:22 +0100

    game_start.c 1.1 : Ajustement graphique

--- sources/game_start.c
+++ sources/game_start.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 14:09:49 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 15:58:15 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/19 17:41:50 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		game_start(t_2048 *wkw)
 	{
 		clear();
 		wkw->wdow = subwin(stdscr, LINES, COLS, 0, 0);
-		wborder(wkw->wdow, '/','/',  '/' , '/', '/','/', '/','/');
+		wborder(wkw->wdow, '|','|',  '-' , '-', '+','+', '+','+');
 		move(LINES * 0.5, COLS * 0.5 - ft_strlen("Welcome to 2048 ! \
 			Press Enter to play") / 2 + 2);
 		printw("Welcome to 2048 ! Press Enter to play");
