963ab43418c0829a34ae07f94d87e2a1acdd8691
Mathis Pandelé <mapandel@student.42.fr>
2017-03-21 22:28:05 +0100

    game_down.c 1.3 : Gameplay fix

--- sources/game_down.c
+++ sources/game_down.c
@@ -6,13 +6,13 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 12:07:08 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 21:42:07 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/21 22:23:20 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
 
-static void		game_down2(t_2048 *wkw, int *tmp, int *count)
+static void		game_down2(t_2048 *wkw, int *tmp, int *count, int *lock)
 {
 	if (*tmp >= 4 && (++*count))
 	{
@@ -20,7 +20,7 @@ static void		game_down2(t_2048 *wkw, int *tmp, int *count)
 			[wkw->columns];
 		wkw->map[wkw->lines][wkw->columns] = 0;
 	}
-	else if (wkw->map[*tmp][wkw->columns] == wkw->map[wkw->lines]
+	else if (!*lock && wkw->map[*tmp][wkw->columns] == wkw->map[wkw->lines]
 			[wkw->columns] && (++*count))
 	{
 		wkw->map[*tmp][wkw->columns] = wkw->map[wkw->lines]
@@ -28,6 +28,7 @@ static void		game_down2(t_2048 *wkw, int *tmp, int *count)
 		wkw->score += (size_t)wkw->map[wkw->lines]
 			[wkw->columns] * 2;
 		wkw->map[wkw->lines][wkw->columns] = 0;
+		++*lock;
 	}
 	else if (*tmp - 1 != wkw->lines && (++count))
 	{
@@ -41,12 +42,14 @@ int				game_down(t_2048 *wkw)
 {
 	int		tmp;
 	int		count;
+	int		lock;
 
 	count = 0;
 	wkw->columns = 0;
 	while (wkw->columns < 4)
 	{
 		wkw->lines = 2;
+		lock = 0;
 		while (wkw->lines >= 0)
 		{
 			if (wkw->map[wkw->lines][wkw->columns] != 0)
@@ -54,7 +57,7 @@ int				game_down(t_2048 *wkw)
 				tmp = wkw->lines + 1;
 				while (tmp < 4 && wkw->map[tmp][wkw->columns] == 0)
 					++tmp;
-				game_down2(wkw, &tmp, &count);
+				game_down2(wkw, &tmp, &count, &lock);
 			}
 			--wkw->lines;
 		}
