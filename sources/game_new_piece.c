f3bfdaceebcd6acc287c0cc443643875729269dd
Mathis Pandelé <mapandel@student.42.fr>
2017-03-21 22:26:53 +0100

    game_new_piece.c 1.1.3 : Gameplay fix

--- sources/game_new_piece.c
+++ sources/game_new_piece.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 12:20:53 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/21 08:21:23 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/21 22:04:22 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void			game_new_piece(t_2048 *wkw)
 {
 	int		tmp;
 
-	tmp = 2;
+	tmp = 4;
 	while (1)
 	{
 		wkw->lines = rand() % 4;
@@ -24,7 +24,7 @@ void			game_new_piece(t_2048 *wkw)
 		if (wkw->map[wkw->lines][wkw->columns] == 0)
 		{
 			if (rand() % 4)
-				tmp = 4;
+				tmp = 2;
 			wkw->map[wkw->lines][wkw->columns] = tmp;
 			break ;
 		}
