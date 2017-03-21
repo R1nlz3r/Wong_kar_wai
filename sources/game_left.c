1c35b88ecf146936f2467d888a19e52678b87258
Mathis Pandelé <mapandel@student.42.fr>
2017-03-21 22:28:14 +0100

    game_left.c 1.3 : Gameplay fix

--- sources/game_left.c
+++ sources/game_left.c
@@ -6,13 +6,13 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/19 12:39:09 by mapandel          #+#    #+#             */
-/*   Updated: 2017/03/19 21:43:04 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/03/21 22:24:55 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
 
-static void		game_left2(t_2048 *wkw, int *tmp, int *count)
+static void		game_left2(t_2048 *wkw, int *tmp, int *count, int *lock)
 {
 	if (*tmp < 0 && (++*count))
 	{
@@ -20,7 +20,7 @@ static void		game_left2(t_2048 *wkw, int *tmp, int *count)
 			[wkw->columns];
 		wkw->map[wkw->lines][wkw->columns] = 0;
 	}
-	else if (wkw->map[wkw->lines][*tmp] == wkw->map[wkw->lines]
+	else if (!*lock && wkw->map[wkw->lines][*tmp] == wkw->map[wkw->lines]
 			[wkw->columns] && (++*count))
 	{
 		wkw->map[wkw->lines][*tmp] = wkw->map[wkw->lines]
@@ -28,6 +28,7 @@ static void		game_left2(t_2048 *wkw, int *tmp, int *count)
 		wkw->score += (size_t)wkw->map[wkw->lines]
 			[wkw->columns] * 2;
 		wkw->map[wkw->lines][wkw->columns] = 0;
+		++*lock;
 	}
 	else if (*tmp + 1 != wkw->columns && (++count))
 	{
@@ -41,12 +42,14 @@ int				game_left(t_2048 *wkw)
 {
 	int		tmp;
 	int		count;
+	int		lock;
 
 	count = 0;
 	wkw->lines = 0;
 	while (wkw->lines < 4)
 	{
 		wkw->columns = 1;
+		lock = 0;
 		while (wkw->columns < 4)
 		{
 			if (wkw->map[wkw->lines][wkw->columns] != 0)
@@ -54,7 +57,7 @@ int				game_left(t_2048 *wkw)
 				tmp = wkw->columns - 1;
 				while (tmp >= 0 && wkw->map[wkw->lines][tmp] == 0)
 					--tmp;
-				game_left2(wkw, &tmp, &count);
+				game_left2(wkw, &tmp, &count, &lock);
 			}
 			++wkw->columns;
 		}
